e33bba052f36222e32efb26c0cba6fec975076e8
Leandro Urquiza <leanurquiza@MacBook-Pro-de-Leandro.local>
2024-10-17 19:33:47 -0300

    se agrega bootstrap y css

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from inicio.views import mi_vista,inicio,vista_datos_1,primer_template,segundo_template,tercer_template,cuarto_template
+from inicio.views import mi_vista,crear_auto,template_2,inicio
 
 urlpatterns = [
     path('mi-vista/',mi_vista),
-    path('',inicio),
-    path('vista-datos1/<nombre>/',vista_datos_1),
-    path('primer-template',primer_template),
-    path('segundo-template',segundo_template),
-    path('tercer-template',tercer_template),
-    path('cuarto-template',cuarto_template)
+    path('crear-auto/<marca>/<modelo>/<anio>',crear_auto),
+    path('cuarto-template',template_2),
+    path('inicio/',inicio)
     
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -2,67 +2,26 @@ from django.http import HttpResponse
 from django.template import Template,Context,loader
 from datetime import datetime
 from django.shortcuts import render
+from inicio.models import Auto
 
 def mi_vista (request):
     return HttpResponse('hola soy mi vista')
 
-
-def inicio (request):
-    return HttpResponse('<h1>Inicio Principal</h1>')
-
-
-def vista_datos_1 (request,nombre):
-    nombre_mayuscular=nombre.upper()
-    return HttpResponse(f'Hola {nombre_mayuscular}')
-
-def primer_template(request):
-    
-    archivo_del_template=open(r'templates/primer_template.html','r')
-    
-    template=Template(archivo_del_template.read())
-    
-    archivo_del_template.close()
-    contexto= Context()
-    
-    render_template=template.render(contexto)
-    
-    return HttpResponse(render_template)
-
-
-def segundo_template(request):
-    
-    fecha_actual=datetime.now()
-    
-    archivo_del_template=open(r'templates/segundo_template.html','r')
-    
-    template=Template(archivo_del_template.read())
-    
-    archivo_del_template.close()
-    
-    datos={'fecha_actual':fecha_actual}
-    
-    contexto= Context(datos)
-    
-    render_template=template.render(contexto)
-    
-    return HttpResponse(render_template)
-
-def tercer_template(request):
-    
-    fecha_actual=datetime.now()
-    datos={'fecha_actual':fecha_actual}
-    
-    template=loader.get_template('tercer_template.html')
-    
-    render_template=template.render(datos)
-
-    return HttpResponse(render_template)    
-
-
-def cuarto_template(request):
+def template_2(request):
     fecha_actual=datetime.now()
     datos={'fecha_actual':fecha_actual,
            'numeros':list(range(1,11))}
     
     
     return render(request,'cuarto_template.html',datos)
+
+def crear_auto(request,marca,modelo,anio):
+    
+    auto=Auto(marca=marca,modelo=modelo,anio=anio)
+    auto.save()
+
+
+    return render(request,'crear_auto.html',{'auto':auto})
+
+def inicio(request):
+    return render(request,'index.html')
